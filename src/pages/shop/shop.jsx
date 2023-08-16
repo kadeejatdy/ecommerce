fd7adcba273192804d0208c6fe6f323e4bc67bc3
kadeejatdy <kadeejatdy@gmail.com>
2023-08-16 06:49:29 +0530

    files aded

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Product } from './product'
+import {PRODUCTS} from "../../products"
+import"./shop.css"
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        <div className='shopTitle'>
+            <h1>Tech Shop</h1>
+        </div>
+        <div className='products'>
+            {""}
+            {PRODUCTS.map((product)=>(
+        <Product data={product}/>)
+        )}
+
+        </div>
+
+
+    </div>
+  )
+}
